3caff3ca3ecd24a35361e46cda8b525877d279d2
Ville Pakarinen <151925563+vpakarinen2@users.noreply.github.com>
2025-12-18 13:26:18 +0200

    Add backend db file

---
+++ backend/app/db.py
@@ -0,0 +1,20 @@
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+from app.settings import settings
+
+
+connect_args = {}
+if settings.database_url.startswith("sqlite"):
+    connect_args = {"check_same_thread": False}
+
+engine = create_engine(settings.database_url, connect_args=connect_args)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
